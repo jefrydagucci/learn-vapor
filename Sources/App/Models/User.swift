f070e73b49bfdcf0c5ea528d4662556af2a60c56
Jefry Eko Mulya <jefrydagucci@gmail.com>
2020-01-16 14:44:21 +0800

    [MODEL] create model user

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,33 @@
+///**
+/**
+
+Created by: Jefry Eko Mulya on 16/01/20
+https://github.com/jefrydagucci
+Copyright (c) 2020 DAGUCI
+
+*/
+
+import Fluent
+import Vapor
+
+final class User: Model, Content {
+    static let schema = "users"
+    
+    @ID(key: "id")
+    var id: Int?
+    
+    @ID(key: "name")
+    var name: String
+    
+    @ID(key: "username")
+    var username: String
+
+    init() { }
+
+    init(id: Int? = nil, name: String, username: String) {
+        self.id = id
+        self.name = name
+        self.username = username
+    }
+}
+
