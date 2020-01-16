16e091c7e0a951e1285edf73fb00f4a2ec3e5dd6
Jefry Eko Mulya <jefrydagucci@gmail.com>
2020-01-16 14:11:05 +0800

    [REQUEST] create count bbottles request

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,14 @@ func routes(_ app: Application) throws {
     app.get("hello") { req in
         return "Hello, world!"
     }
+    
+    //bottles/99
+    app.get("bottles", ":count") { req -> String in
+        guard let count = req.parameters.get("count", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "There were \(count) bottles on the wall"
+    }
 
     let todoController = TodoController()
     app.get("todos", use: todoController.index)
