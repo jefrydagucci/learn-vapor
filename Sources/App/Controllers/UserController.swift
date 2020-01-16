67983733656beb8037001931942da82c454f5473
Jefry Eko Mulya <jefrydagucci@gmail.com>
2020-01-16 15:53:41 +0800

    [USER] [TABLE] Create get user handler

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -21,11 +21,29 @@ struct UserController: RouteCollection {
     */
     func boot(routes: RoutesBuilder) throws {
         routes.post("api", "users", use: createHandler)
+        routes.get("api", "users", use: getAllHandler)
     }
     
     func createHandler(req: Request) throws -> EventLoopFuture<User> {
         let user = try req.content.decode(User.self)
         return user.save(on: req.db).map { user }
+    }
+    
+    /*
+    GET http://localhost:8080/api/users/
+    [
+     {
+        "name" : "Jefry",
+        "username": "jefrydagucci"
+     },
+     {
+        "name" : "Jefry2",
+        "username": "jefrydagucci2"
+     }
+     ]
+    */
+    func getAllHandler(req: Request) throws -> EventLoopFuture<[User]> {
+        return User.query(on: req.db).all()
     }
 }
 
