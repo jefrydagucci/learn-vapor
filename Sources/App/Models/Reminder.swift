ceea276e0d13c3d509310bd542ab42cadc7861c5
Jefry Eko Mulya <jefrydagucci@gmail.com>
2020-01-16 16:37:20 +0800

    [RELATIONSHIP] Create model relationship

---
+++ Sources/App/Models/Reminder.swift
@@ -0,0 +1,32 @@
+///**
+/**
+
+Created by: Jefry Eko Mulya on 16/01/20
+https://github.com/jefrydagucci
+Copyright (c) 2020 DAGUCI
+
+*/
+
+import Fluent
+import Vapor
+
+final class Reminder: Model, Content {
+    static let schema = "reminders"
+    
+    @ID(key: "id")
+    var id: Int?
+    
+    @Field(key: "title")
+    var title: String
+    
+    @Parent(key: "userID")
+    var user: User
+
+    init() { }
+
+    init(id: Int? = nil, title: String, userID: User.IDValue) {
+        self.id = id
+        self.title = title
+        self.$user.id = userID
+    }
+}
